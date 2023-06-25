c807227b787767e0cd24d9c1f7137fcb14d2bfbe
Pablo A <pcaa3000@gmail.com>
2023-06-25 12:08:46 -0500

    Update GLPI v10.0.7

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker exec soporte_mariadb-glpi_1 sh -c 'exec mysqldump glpi -uroot -pAideVezuque8afohka1lutaeniemi6ei' | gzip -9 > /home/soporte/backups/glpi-backup-$(date +%Y-%m-%d-%H-%M).sql.gz
-find /home/soporte/backups/ -mtime +30 -delete
+docker exec ${USER}_mariadb-glpi_1 sh -c 'exec mysqldump glpi -uroot -pmyStr0ngP@sSW0rd' | gzip -9 > $PWD/backups/glpi-backup-$(date +%Y-%m-%d-%H-%M).sql.gz
+find $PWD/backups/ -mtime +30 -delete
